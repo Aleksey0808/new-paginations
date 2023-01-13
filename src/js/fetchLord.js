c13f15adfe19a6cff2b030a4a518abca837d7e6f
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-01-13 21:02:50 +0200

    add marcup

---
+++ src/js/fetchLord.js
@@ -0,0 +1,15 @@
+export const obj = {
+  user: "name"
+};
+
+function searhVCharacters (pageNumber = 1) {
+  const URL = `https://the-one-api.dev/v2/character?limit=20&page=${pageNumber}`
+};
+const options = {
+  headers: {
+    Authorization: "Bearer GAepmfdXiazfYX4VUu4M",
+  },
+};
+return fetch(URL, options).then((r) => r.json())
+
+export default searhVCharacters
---
+++ src/js/pagination.js
@@ -0,0 +1,49 @@
+import fetchLord from './fetchLord';
+import creatMarkup from './createMarkup';
+
+const paginationBox = document.querySelector('.pagination');
+let globalCurrentPage = 0;
+
+// @param {Number} currentPage
+// @param {Number} allPage
+// return {String} markup
+
+export default function pagination(currentPage, allPages) {
+  let markup = '';
+  let beforeTwoPage = currentPage - 2;
+  let beforePage = currentPage - 1;
+  let afterPage = currentPage + 1;
+  let afterTwoPage = currentPage + 2;
+  globalCurrentPage = currentPage;
+
+  if (currentPage > 1) {
+    markup += `<li>&#129144;</li>`;
+    markup += `<li>1</li>`;
+  }
+  if (currentPage > 4) {
+    markup += `<li>...</li>`;
+  }
+  if (currentPage > 3) {
+    markup += `<li>${beforeTwoPage}</li>`;
+  }
+  if (currentPage > 2) {
+    markup += `<li>${beforePage}</li>`;
+  }
+  markup += `<li><b>${currentPage}</b></li>`;
+
+  if (allPages - 1 > currentPage) {
+    markup += `<li>${afterPage}</li>`;
+  }
+  if (allPages - 2 > currentPage) {
+    markup += `<li>${afterTwoPage}</li>`;
+  }
+  if (allPages - 3 > currentPage) {
+    markup += `<li>...</li>`;
+  }
+
+  if (allPages > currentPage) {
+    markup += `<li>${allPages}</li>`;
+    markup += `<li>&#129146;</li>`;
+  }
+  paginationBox.innerHTML = markup;
+}
